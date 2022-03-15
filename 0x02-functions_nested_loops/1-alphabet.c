190e3d74a5f2231febdc8ea0f75211b5b82be697
Latheresienne <therefeu12@gmail.com>
2022-03-15 08:10:26 -0700

    Compile program

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,20 @@
+#include <unistd.h>
 #include "main.h"
 
 /**
- * main- amin entry
- * Return: Always 0.
+ * print_alphabet - does my stuff
+ * No argument
+ * Description: prints the alphabet in lowercase.
  */
 
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		_putchar(alphabet[i]);
+	}
+	_putchar('\n');
 }
