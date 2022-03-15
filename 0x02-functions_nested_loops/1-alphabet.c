41097247a01ceffa651b44ff65ce465d565e968c
Latheresienne <therefeu12@gmail.com>
2022-03-15 04:59:00 -0700

    Prints the alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main- amin entry
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib>
+#include "main.h"
+
+/**
+ * print_alphabet - does my stuff
+ * No argument
+ * Description: prints the alphabet in lowercase.
+ */
+
+void print_alphabet(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 27; i++)
+	{
+		_putchar(alphabet[i];
+	}
+	_putchar("\n");
+}
