7c40cfd23aa90ee048bca4b7928b895ce2a33170
Latheresienne <therefeu12@gmail.com>
2022-03-18 05:54:51 -0700

    Fixed check 3

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -17,24 +17,16 @@ int main(void)
 		max_prime = 2;
 		n /= 2;
 	}
-	while (n % 3 == 0)
-	{
-		max_prime = 3;
-		n /= 3;
-	}
-	for (i = 5; i <= sqrt(n); i += 6)
+	for (i = 3; i <= sqrt(n); i += 2)
 	{
 		while (n % i == 0)
 		{
 			max_prime = i;
 			n = n / i;
 		}
-		while (n % (i + 2) == 0)
-		{
-			max_prime = i + 2;
-			n /= (i + 2);
-		}
 	}
+	if ( n > 2)
+		max_prime = n;
 	printf("%ld\n", max_prime);
 	return (0);
 }
