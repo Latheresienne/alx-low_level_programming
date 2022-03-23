fcd44f37bfb32210dbb7774bb68607e3cc413937
Latheresienne <therefeu12@gmail.com>
2022-03-23 09:28:39 -0700

    4task 1 completed

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - Swap the values of its parameters. 
+ * @a: A pointer to an integer
+ * @b: A pointer to an integer
+ * Description: Swap the values of 0 and b.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int aux;
+	aux = *a;
+	*a = *b;
+	*b = aux;
+}
